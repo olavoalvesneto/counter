fa4da8791b55f93a2bad9d2a609baa7787aec683
Olavo Alves <olavoalvesneto@hotmail.com>
2024-03-10 14:29:05 -0300

    [SCONTRACT-2] Adds interactions of the contract

--- script/Counter.s.sol
+++ script/Counter.s.sol
@@ -5,9 +5,23 @@ import {Script, console} from "forge-std/Script.sol";
 import {Counter} from "../src/Counter.sol";
 
 contract CounterScript is Script {
+    Counter counter;
+
     function setUp() public {}
 
     function run() public {
         vm.broadcast();
+
+        console.log("ADDRESS :: ", address(counter));
+
+        counter = new Counter;
+        counter.setNumber(99);
+
+        console.log(counter.getValue());
+
+        counter.increment(100);
+
+        console.log(counter.getValue());
+
     }
 }
