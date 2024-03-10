8a82ca33c316bf08dbdc5167d9e41808b90f5153
Olavo Alves <olavoalvesneto@hotmail.com>
2024-03-10 14:21:33 -0300

    [SCONTRACT-2] Imported the contract

--- script/Counter.s.sol
+++ script/Counter.s.sol
@@ -1,7 +1,8 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
 import {Script, console} from "forge-std/Script.sol";
+import {Counter} from "../src/Counter.sol";
 
 contract CounterScript is Script {
     function setUp() public {}
