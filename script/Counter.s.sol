0d8103a3fe5fbcf7aa55cda421f3c06f8173e072
Olavo Alves <olavoalvesneto@hotmail.com>
2024-03-10 16:04:56 -0300

    [SCONTRACT-2] Fix script

--- script/Counter.s.sol
+++ script/Counter.s.sol
@@ -1,26 +1,19 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.13;
+pragma solidity ^0.8.24;
 
 import {Script, console} from "forge-std/Script.sol";
 import {Counter} from "../src/Counter.sol";
 
 contract CounterScript is Script {
-    Counter counter;
+    Counter public counter;
 
     function setUp() public {}
 
     function run() public {
-        vm.broadcast(0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80);
-
-        console.log("ADDRESS :: ", address(counter));
-
+        vm.startBroadcast(0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80);
         counter = new Counter();
         counter.setNumber(99);
-        console.log(counter.getValue());
-
-        counter.increment();
-        console.log(counter.getValue());
-
-        vm.broadcast();
+        console.log("ADDRESS :: ", address(counter));
+        vm.stopBroadcast();
     }
 }
